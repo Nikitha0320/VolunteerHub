3b7bba2844406bc1f94ee40d2d1aea938bd08eb1
Nikitha0320 <nikithareddygujjuri@gmail.com>
2026-03-23 14:50:35 +0530

    commit

--- backend/src/main/java/com/vms/backend/VolunteerServiceManagementSystemApplication.java
+++ backend/src/main/java/com/vms/backend/VolunteerServiceManagementSystemApplication.java
@@ -9,7 +9,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class VolunteerServiceManagementSystemApplication {
 
     public static void main(String[] args) {
-        
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(VolunteerServiceManagementSystemApplication.class, args);
